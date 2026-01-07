06b2c3b922d9cb6f2393e7e831be0a6b8bd008d3
chetan kushwah <chetankushwah929@gmail.com>
2026-01-08 02:39:25 +0530

    water plant site build error fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { Phone, Droplets, Shield, Calendar } from 'lucide-react';
+import { Phone, Droplets, Shield } from 'lucide-react';
 import heroImage from '../assets/image.png';
 import heroImage1 from '../assets/bottles-conveyer.png';
 
